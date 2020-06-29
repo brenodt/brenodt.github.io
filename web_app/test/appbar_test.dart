5d1282a319a85e136d70409efe3c72aeeda42e65
Breno Teodoro <54450520+brenodt@users.noreply.github.com>
2020-06-28 22:44:47 -0300

    #1 Added test to ascertain if SliverAppBar is not pinned to the top

--- web_app/test/appbar_test.dart
+++ web_app/test/appbar_test.dart
@@ -4,16 +4,39 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:web_app/main.dart';
 
 void main() {
-  testWidgets('Tests if SliverAppBar collapses after scrolling',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
+  /// This group of tests require the remainder of the app body to be
+  /// sufficiently scrollable to have the SliverAppBar collapsed and reopened.
+  group('Tests SliverAppBar behavior', () {
+    testWidgets('Tests if SliverAppBar collapses after scrolling.',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(MyApp());
 
-    expect(find.byType(SliverAppBar), findsOneWidget);
+      expect(find.byType(SliverAppBar), findsOneWidget);
 
-    final gesture = await tester.startGesture(Offset(0, 300));
-    await gesture.moveBy(Offset(0, -300));
-    await tester.pump();
+      final gesture = await tester.startGesture(Offset(0, 300));
+      await gesture.moveBy(Offset(0, -300));
+      await tester.pump();
 
-    expect(find.byType(SliverAppBar), findsNothing);
+      expect(find.byType(SliverAppBar), findsNothing);
+    });
+
+    testWidgets('''
+    Tests if SliverAppBar reappears after scrolling down and rescrolling up.
+    ''', (WidgetTester tester) async {
+      await tester.pumpWidget(MyApp());
+
+      expect(find.byType(SliverAppBar), findsOneWidget);
+
+      final gesture = await tester.startGesture(Offset(0, 300));
+      await gesture.moveBy(Offset(0, -600));
+      await tester.pump();
+
+      expect(find.byType(SliverAppBar), findsNothing);
+
+      await gesture.moveBy(Offset(0, 300));
+      await tester.pump();
+
+      expect(find.byType(SliverAppBar), findsOneWidget);
+    });
   });
 }
