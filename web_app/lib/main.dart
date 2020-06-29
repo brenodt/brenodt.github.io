0355af0e32fbef3e49d62dccb8a14523f772ff2e
Breno Teodoro <54450520+brenodt@users.noreply.github.com>
2020-06-28 22:32:48 -0300

    #1 Added CustomScroll view to body and SliverAppBar

--- web_app/lib/main.dart
+++ web_app/lib/main.dart
@@ -38,22 +38,22 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text(widget.title),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Container(
+                  height: 1000,
+                  color: Colors.teal,
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
