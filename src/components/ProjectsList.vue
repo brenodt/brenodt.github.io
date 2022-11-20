b45651297d401adf433b93711e6260858b169cc1
brenodt <54450520+brenodt@users.noreply.github.com>
2022-11-20 10:44:27 -0300

    feat: v1.0.0

---
+++ src/components/ProjectsList.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import ProjectItem from "./ProjectItem.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+</script>
+
+<template>
+  <ProjectItem>
+    <template #icon>
+      <font-awesome-icon icon="fa-solid fa-comments" />
+    </template>
+    <template #heading>Qaul</template>
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida
+    sollicitudin massa sit amet convallis. Nullam eget pulvinar nibh.
+  </ProjectItem>
+
+  <ProjectItem>
+    <template #icon>
+      <font-awesome-icon icon="fa-solid fa-utensils" />
+    </template>
+    <template #heading>GoCoCo</template>
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida
+    sollicitudin massa sit amet convallis. Nullam eget pulvinar nibh.
+  </ProjectItem>
+
+  <ProjectItem>
+    <template #icon>
+      <font-awesome-icon icon="fa-solid fa-headphones-simple" />
+    </template>
+    <template #heading>Mindbliss</template>
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida
+    sollicitudin massa sit amet convallis. Nullam eget pulvinar nibh.
+  </ProjectItem>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h3 class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida
+      sollicitudin massa sit amet convallis. Nullam eget pulvinar nibh.
+    </h3>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+
+.content {
+  margin-top: 40px;
+}
+</style>
